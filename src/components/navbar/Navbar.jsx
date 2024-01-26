9dde040ea6d25247ddd9b450589f8e88e2f57814
Francisco Zuidwijk <electrocisko@gmail.com>
2024-01-26 18:15:16 -0300

    working in mobil styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,45 +1,75 @@
-import './navbar.css'
-import {NavLink}from 'react-router-dom'
+import { useState } from "react";
+import "./navbar.css";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-  <nav className="nav">
-    <ul>
-    <li>
-        <NavLink to="/"><img src="./src/assets/images/logo.svg" alt="" /></NavLink>
-      </li>
-      <li>
-        <NavLink to="/features">Features <img src="./src/assets/images/icon-arrow-down.svg" alt="Icon Arrow down" /></NavLink>
-      </li>
-      <li>
-        <NavLink to="/company">Company <img src="./src/assets/images/icon-arrow-down.svg" alt="Icon Arrow down" /></NavLink>
-      </li>
-      <li>
-        <NavLink to="/careers">Careers</NavLink>
-      </li>
-      <li>
-        <NavLink to="/about">About</NavLink>
-      </li>
-      <div className="session">
-      <li>
-        <NavLink to="/login">Login</NavLink>
-      </li>
-      <li >
-        <NavLink className="register" to="/register">Register</NavLink>
-      </li>
+    <nav>
+      <div className="nav-mobil">
+        <li>
+          <Link to="/" className="title">
+            <img src="./src/assets/images/logo.svg" alt="Logo" />
+          </Link>
+        </li>
+
+        <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+          <img src="./src/assets/images/icon-menu.svg" alt="Logo" />
+        </div>
       </div>
 
-    </ul>
-  </nav>
-  )
+      <ul className={menuOpen ? "open" : "closed"}>
+        <div className="menu_container">
+          <div onClick={() => setMenuOpen(!menuOpen)}>
+            <img
+            className="close-menu"
+              src="./src/assets/images/icon-close-menu.svg"
+              alt="Close Icon"
+            />
+          </div>
+
+          <div className="links-container">
+            <li>
+              <NavLink to="/features">
+                Features{" "}
+                <img
+                  src="./src/assets/images/icon-arrow-down.svg"
+                  alt="Icon Arrow down"
+                />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/company">
+                Company{" "}
+                <img
+                  src="./src/assets/images/icon-arrow-down.svg"
+                  alt="Icon Arrow down"
+                />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/careers">Careers</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+          </div>
+
+          <div className="session-container">
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+            <li>
+              <NavLink className="register" to="/register">
+                Register
+              </NavLink>
+            </li>
+          </div>
+        </div>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
-
-
-  
-
-
-
-  
-  
+export default Navbar;
