902dbb8b001deaa7ac0ec11afb8082a5c9a01db7
Francisco Zuidwijk <electrocisko@gmail.com>
2024-01-27 18:56:33 -0300

    working in main component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,10 @@ import styles from './footer.module.scss'
 function Footer() {
   return (
     <div className={styles.attribution}>
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-    Coded by <a href="https://github.com/Electrocisko">Francisco J. Zuidwijk</a>.
+      <p> Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a> </p>
+      <p>  Coded by <a href="https://github.com/Electrocisko">Francisco J. Zuidwijk</a></p>
+   
+  
   </div>
   )
 }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,15 +1,29 @@
-import styles from './main.module.scss'
+import styles from "./main.module.scss";
 
 function Main() {
   return (
-  <>
-<div >
-<img src="./src/assets/images/image-hero-mobile.png" alt="" className={styles.image}/>
-</div>
-  
-  
-  </>
-  )
+    <div className={styles.main_container}>
+      <div>
+        <img
+          src="./src/assets/images/image-hero-mobile.png"
+          alt=""
+          className={styles.image}
+        />
+      </div>
+      <h1 className={styles.title}>Make remote work</h1>
+      <p className={styles.text}>
+        Get your team in sync, no matter your location. Streamline processes,
+        create team rituals, and watch productivity soar.
+      </p>
+      <button className={styles.button}>Learn more</button>
+      <div className={styles.images_container}>
+        <img src="./src/assets/images/client-databiz.svg" alt="Databiz" height={"15px"} />
+        <img src="./src/assets/images/client-audiophile.svg" alt="Audiophile" height={"20px"}/>
+        <img src="./src/assets/images/client-meet.svg" alt="Meet" height={"15px"}/>
+        <img src="./src/assets/images/client-maker.svg" alt="Maker" height={"15px"} />
+      </div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
